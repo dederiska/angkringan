3878c9bbbea5fe09fe8f9fe32ca2a195a8381a00
Afifsuryaramadhan1 <afifsuryaramadhan@gmail.com>
2022-01-28 08:43:45 +0700

    reset_m model edit

--- application/models/Reset_m.php
+++ application/models/Reset_m.php
@@ -5,7 +5,7 @@ class Reset_m extends CI_Model
     public function update_reset_key($email, $reset_key)
     {
         $this->db->where('email', $email);
-        $data = array('reset_password' => $reset_key);
+        $data = array('password' => $reset_key);
         $this->db->update('user', $data);
         if ($this->db->affected_rows() > 0) {
             return true;
